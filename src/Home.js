9eadb3d94aeabc30363374250b07186bb140f892
Ronihe <herongrong2011@gmail.com>
2019-01-27 18:09:14 -0800

    need to style it

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import coding from './img/coding.jpg';
-import Contact from './Contact';
 
 const Wrapper = styled.div`
   margin-bottom: 8.5em;
